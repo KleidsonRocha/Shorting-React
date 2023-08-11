485ee77adf03d0a65178918fdfdc876003dd63b0
kleidson matos da rocha <kleidsonmr@gmail.com>
2023-08-11 16:13:18 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Grid, Stack, styled } from '@mui/material'
 import { useEffect, useState } from 'react'
 import './App.css'
 import { NavBar } from './components/core/Navbar'
 import { Page } from './types/page'
+import Footer from './components/core/Footer'
+
+
 
 function App() {
     const [arraySize, setArraySize] = useState<number>(10)
@@ -19,13 +23,35 @@ function App() {
             />
 
             { page === 'home' && (
-                <div>home</div>
+                <>
+                    <Grid container spacing={2}>
+                        <Grid item xs={8}>
+                            <h1>Escolha do Assunto</h1>
+                            <text>kleidson</text>
+                        </Grid>
+                        <Grid alignItems="flex-start" item xs={4}>
+                            <h1>Participantes</h1>
+                            <text>Kleidson (Progamacaoo e Pesquisa)</text><br />
+                            <text>Matheus (Pesquisa)</text><br />
+                            <text>Luis (Design)</text><br />
+                        </Grid>
+                    </Grid>
+                    <Footer />
+                </>
             )}
             { page === 'bubble' && (
-                <div>bubble</div>
+                <>
+                    <div>bubble</div>
+                    <Footer />
+
+                </>
             )}
             { page === 'shell' && (
-                <div>shell</div>
+                 <>
+                 <div>Shell</div>
+                 <Footer />
+
+             </>
             )}
         </>
     );
